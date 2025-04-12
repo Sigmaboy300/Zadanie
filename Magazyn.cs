28988c6cbb33584ec1bf5063066baf24d5e574df
Sigmaboy300 <artur.krefta@teb.edu.pl>
2025-04-12 16:12:45 +0200

    Add files via upload

---
+++ Magazyn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+
+        public Product(string name, int quantity, double unitPrice)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public Product() { }
